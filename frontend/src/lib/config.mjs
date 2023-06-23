018262c1730bb7b3864b5ed44da53386c7736776
Gabriel D <gabrieldavidfs@hotmail.com>
2023-06-23 19:56:04 +0200

    Added Backend URL

--- frontend/src/lib/config.mjs
+++ frontend/src/lib/config.mjs
@@ -1,6 +1,6 @@
 const baseUrl = ["localhost", "127.0.0.1"].includes(window.location.hostname) 
     ? "http://localhost:8000" 
-    : ""
+    : "https://backend-5mb6.onrender.com"
 
 const pathAPIVersion = "/api/v1.0"
 
